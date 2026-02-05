e466f50b8c24e55113901987dd3837e476b644c8
WeiqiGO8 <hannah.lingzhi@gmail.com>
2026-02-05 16:59:18 +0100

    working on lab 3 task 4

--- public/js/start.js
+++ public/js/start.js
@@ -1,1 +1,15 @@
 console.log("hello");
+
+function fetchProducts() {
+	fetch("/api/products")
+		.then((response) => response.json())
+		.then((data) => {
+			//data
+			console.log(data);
+		})
+		.catch((error) => console.log(error));
+}
+
+function fetchProductsByID() {}
+
+fetchProducts();
--- server.js
+++ server.js
@@ -4,10 +4,31 @@ const PORT = 3000;
 
 app.use("/", express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/welcome", (req, res) => {
 	res.send("Welcome to the REST API!");
+});
+
+app.get("/api/products", (req, res) => {
+	res.json(products); // Converts Javascript data into JSON and sends it back
+});
+
+app.get("/api/products/:id", (req, res) => {
+	const rid = parseInt(req.params.id);
+	console.log(rid);
+	// Loop over products and take out the respective object !!
+	for (let product of products) {
+		console.log(product);
+	}
+
+	res.json(product);
 });
 
 app.listen(PORT, () => {
 	console.log(`server running at http://localhost:${PORT}`);
 });
+
+//Global object that any function can access.
+let products = [
+	{ id: 1, name: "Laptop", price: 1000 },
+	{ id: 2, name: "Phone", price: 500 },
+];
