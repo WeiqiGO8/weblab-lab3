35a3b25de2cdcabe7aef92537b0ec08e2c65a8b1
WeiqiGO8 <hannah.lingzhi@gmail.com>
2026-02-18 12:51:40 +0100

    lab 3 part 1 task 4 needed fix, it's fixed
    lab 3 part 2 task 1 is done

--- public/js/start.js
+++ public/js/start.js
@@ -1,12 +1,21 @@
 console.log("hello");
 
 //! Lab 3 part 1 task 4
+//* Building GET Endpoints
+
 function fetchProducts() {
 	fetch("/api/products")
 		.then((response) => response.json())
 		.then((data) => {
 			//data
 			console.log(data);
+			// Loop over products and take out the respective object !!
+			console.log(`get response data`, data);
+			data.forEach((p) => {
+				let newLi = document.createElement("li");
+				newLi.innerText = p.id + ": " + p.name + ", " + p.price;
+				document.querySelector(`#insertProduct`).appendChild(newLi);
+			});
 		})
 		.catch((error) => console.log(error));
 }
@@ -24,4 +33,29 @@ function fetchProductsByID() {
 		});
 }
 
-fetchProducts(id);
+//fetchProducts();
+
+//! Lab 3 part 2 task 1
+//* Handling POST requests
+let submitBtn = document.querySelector("#submitBtn");
+
+function addProduct() {
+	fetch("/api/products", {
+		method: `POST`,
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({
+			productName: "NewProduct",
+			productPrice: 100,
+		}),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			console.log("Post response data:", data);
+
+			// let newLi = document.createElement("li");
+			// newLi.innerText = p.id + ": " + p.name + ", " + p.price;
+			// document.querySelector(`#insertProduct`).appendChild(newLi);
+		});
+}
+
+addProduct();
--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.get("/welcome", (req, res) => {
 });
 
 //! Lab 3 part 1 task 4
+//* Building GET Endpoints
+
+//Global object that any function can access.
+let products = [
+	{ id: 1, name: "Laptop", price: 1000 },
+	{ id: 2, name: "Phone", price: 500 },
+];
+
+//building get endpoints
 app.get("/api/products", (req, res) => {
 	res.json(products); // Converts Javascript data into JSON and sends it back
 });
@@ -18,27 +27,40 @@ app.get("/api/products", (req, res) => {
 app.get("/api/products/:id", (req, res) => {
 	const rid = parseInt(req.params.id);
 	console.log(rid);
-
-	// Loop over products and take out the respective object !!
+	// * This works
 	for (let product of products) {
 		if (product.id === rid) {
 			console.log(`Match found`);
 			return res.json(product);
 		}
 	}
-	// 	const product = products.find(p.id === rid);
-
-	// 	if (!product) {
-	// 		return res.status(404).json({ message: `Product not found` });
-	// 	}
 });
 
+//! Lab 3 part 2 task 1
+// * Handling POST requests
+app.post("/api/products", express.json(), (req, res) => {
+	const { productName, productPrice } = req.body;
+	console.log(productName, productPrice);
+	res.json({
+		message: "POST - Created a new item",
+		data: { productName, productPrice },
+	});
+
+	// let newProduct = {
+	// 	id: products.length + 1,
+	// 	name: productName,
+	// 	price: productPrice,
+	// };
+
+	// products.push(newProduct);
+	// res.json(newProduct);
+
+	res.send(
+		`Form received! Product name: ${productName}, product price: ${productPrice}`,
+	);
+});
+
+// PORT/localhost
 app.listen(PORT, () => {
 	console.log(`server running at http://localhost:${PORT}`);
 });
-
-//Global object that any function can access.
-let products = [
-	{ id: 1, name: "Laptop", price: 1000 },
-	{ id: 2, name: "Phone", price: 500 },
-];
